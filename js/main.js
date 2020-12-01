5d4fbbf279571f1c5d8a28b13730f70eefb69415
David Guzman <davidguzmaninc@gmail.com>
2020-12-01 11:15:57 -0500

    Reorganized everything and finish the work section

--- js/main.js
+++ js/main.js
@@ -18,21 +18,46 @@ const navSlide = () => {
     });
 };
 
-const tl = gsap.timeline({
-    defaults: {
-        ease: 'power1.out'
-    }
-});
+const introAnimation = () => {
+    const tl = gsap.timeline({
+        defaults: {
+            ease: 'power1.out'
+        }
+    });
 
-tl.to('.text', { y: '0%', duration: 1, stagger: 0.25 });
-tl.to('.slider', { y: '-100%', duration: 1.5, delay: 0.5 });
-tl.to('.intro', { y: '-100%', duration: 1 }, '-=1');
-tl.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo('#title', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
-tl.fromTo('.lead', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+    tl.to('.text', { y: '0%', duration: 1, stagger: 0.25 });
+    tl.to('.slider', { y: '-100%', duration: 1.5, delay: 0.5 });
+    tl.to('.intro', { y: '-100%', duration: 1 }, '-=1');
+    tl.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
+    tl.fromTo('#title', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+    tl.fromTo('.lead', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+};
+
+const cursorStyle = () => {
+    const mouseCursor = document.querySelector('.cursor');
+    const navLinks = document.querySelectorAll('.nav-links .nav-item a');
+
+    window.addEventListener('mousemove', (e) => {
+        mouseCursor.style.top = `${e.pageY}px`;
+        mouseCursor.style.left = `${e.pageX}px`;
+    });
+
+    navLinks.forEach((link) => {
+        link.addEventListener('mouseover', () => {
+            link.addEventListener('mouseleave', () => {
+                mouseCursor.classList.remove('link-grow');
+                link.classList.remove('hovered-link');
+            });
+            mouseCursor.classList.add('link-grow');
+            link.classList.add('hovered-link');
+        });
+    });
+};
 
 const app = () => {
     navSlide();
+    introAnimation();
+    cursorStyle();
 };
 
 app();
