2542559ea359d54a86670b24b944a71696aee6e8
David Guzman <davidguzmaninc@gmail.com>
2020-11-30 19:14:10 -0500

    Added an animating intro

--- js/main.js
+++ js/main.js
@@ -18,6 +18,19 @@ const navSlide = () => {
     });
 };
 
+const tl = gsap.timeline({
+    defaults: {
+        ease: 'power1.out'
+    }
+});
+
+tl.to('.text', { y: '0%', duration: 1, stagger: 0.25 });
+tl.to('.slider', { y: '-100%', duration: 1.5, delay: 0.5 });
+tl.to('.intro', { y: '-100%', duration: 1 }, '-=1');
+tl.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo('#title', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+tl.fromTo('.lead', { opacity: 0 }, { opacity: 1, duration: 1 }, '-=1');
+
 const app = () => {
     navSlide();
 };
