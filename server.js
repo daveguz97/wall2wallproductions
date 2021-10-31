cd2abf285ff03f96b30c0077833f1941fa76bbd9
David Guzman <davidguzmaninc@gmail.com>
2021-10-30 23:38:54 -0400

    Setup the payments

--- server.js
+++ server.js
@@ -1,20 +1,52 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-const stripePublicKey = process.env.PUBLIC_KEY;
-const stripeSecretKey = process.env.SECRET_KEY;
-
-console.log(stripePublicKey, stripeSecretKey);
-
-const express = require('express');
-
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+const stripe = require("stripe")(process.env.SECRET_KEY);
 
-app.listen(process.env.PORT || 3000);
+// const storeItems = new Map([
+//   [1, { priceInCents: 3000, name: "Basic" }],
+//   // [2, { priceInCents: 5000, name: "Pro" }],
+//   // [3, { priceInCents: 10000, name: "Studio" }],
+// ]);
 
-console.log(`listening on port ${process.env.PORT}`);
+const storeItems = new Map([
+  [1, { priceInCents: 30000, name: "Basic" }],
+  [2, { priceInCents: 50000, name: "Pro" }],
+  [3, { priceInCents: 100000, name: "Studio" }],
+]);
+
+app.post(
+  "wall2wallproductions.netlify.app/create-checkout-session",
+  async (req, res) => {
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        line_items: req.body.items.map((item) => {
+          const storeItem = storeItems.get(item.id);
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: storeItem.name,
+              },
+              unit_amount: storeItem.priceInCents,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${process.env.PUBLIC_URL}/success.html`,
+        cancel_url: `${process.env.PUBLIC_URL}`,
+      });
+      res.json({ url: session.url });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+);
+
+app.listen(3000);
